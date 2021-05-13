c96dfd7dc81fa204c49fe82e0f2f26ab78fc8360
Amitesh Mani Tiwari <amiteshmanitiwari1997@gmail.com>
2021-05-13 08:55:28 +0530

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googlClientID : process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
+    mongoUri : process.env.MONGO_URI,
+    cookiekey: process.env.COOKIE_KEY
+};
